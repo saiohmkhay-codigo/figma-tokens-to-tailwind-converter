0b1f6cf7a5cc2647695d4f1ec3ca31cb0636c877
OhmKhay <chestersai12@gmail.com>
2024-04-23 11:36:21 +0630

    fix: update bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 
+
 const { filterTokensByType } = require("./fns");
 const tokens = require("./output/resolved_map.json")
 
@@ -9,6 +10,16 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors,
+    // spacing: {
+    //   'xxs': 'var(--space-padding-padding-xxs)',
+    //   'xs': 'var(--space-padding-padding-xs)',
+    //   'sm': 'var(--space-padding-padding-sm)',
+    //   'md': 'var(--space-padding-padding-md)',
+    // },
+    // borderRadius: {
+    //   'xxs': 'var(--border-radius-border-radius-xxs)',
+    //   'xs': 'var(--border-radius-border-radius-xs)',
+    // },
   },
   variants: {},
   plugins: [],
